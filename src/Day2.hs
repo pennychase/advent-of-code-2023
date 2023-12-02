97d4e147f9a1cc8e7ee94360a394aed48708a103
Penny Chase <mpchase@mac.com>
2023-12-02 14:25:02 -0500

    Day 2 solutions. Added Parser.hs for MegaParsec utilities.

---
+++ src/Day2.hs
@@ -0,0 +1,94 @@
+module Day2 where
+
+import qualified Data.List as L
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+import Parser
+
+data Color = Red | Green | Blue deriving (Show, Eq, Ord)
+
+strToColor s =
+    case s of
+        "red" -> Red
+        "green" -> Green
+        "blue" -> Blue
+
+data Game = Game Int [[(Int, Color)]] deriving Show
+
+-- Part 1
+-- Given number of each color, which games are possible
+
+-- Game is possible if the number of each color is less than the given numbers
+validCount :: (Int, Color) -> Bool
+validCount (cnt, color) =
+    case color of
+        Red -> cnt <= 12
+        Green -> cnt <= 13
+        Blue -> cnt <= 14 
+
+-- Find all possible games and sum their game numbers
+possible :: [Game] -> Int
+possible = foldr (\g gs -> validGame g + gs) 0
+    where
+        validGame (Game n counts) = 
+            if all validCount (concat counts)
+                then n
+                else 0
+
+-- Part 2
+-- Given a set of games, what is the minimum number of each color that is possible
+
+-- Find the minimum number for each color (i.e., the maximum number across all the "reveals")
+-- and multiple these to obtain the power of the game
+powerOfMinValues :: Game -> Int
+powerOfMinValues (Game _ cnts) =
+    foldr (\y ys ->  (maximum . map fst) y * ys) 1 xs
+    where
+        -- group the counts by Color
+        xs = L.groupBy (\(_,c1) (_,c2) -> c1 == c2) $ L.sortBy (\(_,c1) (_,c2) -> compare c1 c2) $ concat cnts
+
+-- sum the powers for all the games
+sumOfPowers :: [Game] -> Int
+sumOfPowers = foldr (\g gs -> powerOfMinValues g + gs) 0
+
+-- Parsing
+
+
+parseCount :: Parser (Int, Color)
+parseCount = do
+    hspace
+    cnt <- intParser
+    hspace
+    color <- string "red" <|> string "green" <|> string "blue"
+    pure (cnt, strToColor color)
+
+
+parseGame :: Parser Game
+parseGame = do
+    string "Game "
+    game <- intParser
+    char ':'
+    counts <- ((csvParser parseCount) `sepBy` (char ';'))
+    pure $ Game game counts
+
+parseGames :: Parser [Game]
+parseGames = do
+    sepEndBy1 parseGame eol
+
+main :: IO ()
+main = do
+    games <- readInput "./data/day2.txt" parseGames
+    putStrLn $ "Part 1: " <> show (possible games)
+    putStrLn $ "Part 2: " <> show (sumOfPowers games)
+
+testData = concat
+    [ "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n"
+    , "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n"
+    , "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n"
+    , "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n"
+    , "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green\n"
+    ]
---
+++ src/Parser.hs
@@ -0,0 +1,48 @@
+module Parser where
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+import Data.Void
+
+type Parser = Parsec Void String
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme hspace
+
+intParser :: Parser Int
+intParser = lexeme L.decimal
+
+signedInt :: Parser Integer
+signedInt = L.signed hspace L.decimal
+
+csvParser :: Parser a -> Parser [a]
+csvParser p = do
+    vals <- sepEndBy1 p (char ',') 
+    pure vals
+
+pairParser :: Char -> Parser (Int, Int)
+pairParser c = do
+    i <- intParser
+    char c
+    j <- intParser
+    pure (i,j)
+
+lineParser :: Parser a -> Parser [a]
+lineParser parser = sepEndBy1 parser eol
+
+readInput' :: String -> Parser a -> IO a
+readInput' str parser = do
+    case runParser parser "" str of
+        Left _ -> error "Unable to parse input"
+        Right input -> pure input
+
+readInput :: FilePath -> Parser a -> IO a
+readInput path parser = do
+    contents <- readFile path
+    case runParser parser "" contents of
+        Left _ -> error "Unable to parse input"
+        Right input -> pure input
