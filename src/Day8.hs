b3b63e9f906adb9f194d4c01052c6cff39f5652f
Penny Chase <mpchase@mac.com>
2023-12-25 11:17:04 -0500

    Day 8 Part 2 brute force (stack overflow)

--- src/Day8.hs
+++ src/Day8.hs
@@ -13,27 +13,38 @@ import Text.Megaparsec.Char
 
 import Parser
 
+findRoute2 :: Map String (String, String) -> String -> (String -> Bool) -> [String] -> Int
+findRoute2 maps directions endTest starts = go starts directions 0
+    where
+        go s directions cnt =
+            if all endTest s
+                then cnt
+                else go (map (getNext maps (head directions)) s) (tail directions) (cnt + 1)
+
 findRoute :: Map String (String, String) -> String -> (String -> Bool) -> String -> Int
 findRoute maps directions endTest start = go start directions 0
     where
         go s directions cnt =
             if endTest s
                 then cnt
-                else go (getNext s (head directions)) (tail directions) (cnt + 1)
-        getNext cur dir =
-            case next of
+                else go (getNext maps (head directions) s) (tail directions) (cnt + 1)
+
+getNext maps dir cur =
+            case Map.lookup cur maps of
                 Nothing -> error $ "Unknown key: " <> cur
                 Just (left, right) ->
                     if dir == 'L'
                         then left
-                        else right
-            where
-                next = Map.lookup cur maps
-            
+                                else right
+                     
 
 part1 :: String -> Map String (String, String) -> Int
 part1 directions maps =
     findRoute maps (cycle directions) (== "ZZZ") "AAA"
+
+part2 :: String -> Map String (String, String) -> Int
+part2 directions maps =
+    findRoute2 maps (cycle directions) (\x -> last x == 'Z') (filter (\x -> last x == 'A') (Map.keys maps))
 
 
 -- Parsing
@@ -70,7 +81,7 @@ main :: IO ()
 main = do
     (dirs, maps) <- readInput "./data/day8.txt" parseData
     putStrLn $ "Part 1: " <> show (part1 dirs maps)
-
+    putStrLn $ "Part 2: " <> show (part2 dirs maps)
 
 
 testData1 :: Text
@@ -90,3 +101,15 @@ testData2 = T.pack $ "LLR\n\
 \AAA = (BBB, BBB)\n\
 \BBB = (AAA, ZZZ)\n\
 \ZZZ = (ZZZ, ZZZ)\n"
+
+testData3 :: Text 
+testData3 = T.pack $ "LR\n\
+
+\RRA = (RRB, XXX)\n\
+\RRB = (XXX, RRZ)\n\
+\RRZ = (RRB, XXX)\n\
+\QQA = (QQB, XXX)\n\
+\QQB = (QQC, QQC)\n\
+\QQC = (QQZ, QQZ)\n\
+\QQZ = (QQB, QQB)\n\
+\XXX = (XXX, XXX)\n"
