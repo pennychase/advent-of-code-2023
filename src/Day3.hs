18b6162e60af3d04eb0beda6b4959c0752ff1a01
Penny Chase <mpchase@mac.com>
2023-12-26 21:52:06 -0500

    Day 3 Part 1

--- src/Day3.hs
+++ src/Day3.hs
@@ -10,13 +10,15 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
-import Debug.Trace
+import Debug.Trace 
 
 readInt :: String -> Int
 readInt str = read str 
 
-neighbors :: (Int, Int) -> [(Int, Int)]
-neighbors (x,y) = [(x + i, y + j) | j <- [-1,0,1], i <- [-1,0,1]]
+neighbors :: [(Int, Int)] -> S.Set (Int, Int)
+neighbors nums = foldr S.delete (S.unions $ map f nums) nums
+    where
+        f (x, y) = S.fromList [(x + i, y + j) | j <- [-1,0,1], i <- [-1,0,1]]
 
 isSymbol :: Char -> Bool
 isSymbol c 
@@ -25,22 +27,23 @@ isSymbol c
     | otherwise = True
 
 getPartNumber :: Map (Int, Int) Char -> [((Int, Int), Char)] -> Int
-getPartNumber grid nums = 
-    if any isSymbol $ M.mapMaybe (`Map.lookup` grid) $ S.elems $ S.unions $ map (S.fromList . neighbors . fst) nums
+getPartNumber grid nums =  
+    if any isSymbol $ M.mapMaybe (`Map.lookup` grid) $ S.elems $ neighbors (map fst nums)
         then readInt (map snd nums)
         else 0
 
 getParts :: Map (Int, Int) Char -> Int
-getParts grid = go 0 (Map.assocs grid)
+getParts grid = foldr (\x xs -> xs + (oneRow 0 x)) 0 (toRows grid)
     where
-        go :: Int -> [((Int, Int), Char)] -> Int
-        go accum xs =
+        oneRow :: Int -> [((Int, Int), Char)] -> Int
+        oneRow accum xs =
             case L.findIndex (isDigit . snd) xs of
                 Nothing -> accum
                 Just n ->
                     let 
                         (ys, ys') = L.span (isDigit . snd) $ drop n xs
-                    in go (accum + getPartNumber grid ys) ys'
+                    in oneRow (accum + getPartNumber grid ys) ys'
+
 
 makeGrid :: Text -> Map (Int, Int) Char
 makeGrid input = Map.fromList $ zip [ (x,y) | x <- dimX, y <- dimY] (T.unpack (T.concat (T.lines input)))
@@ -74,3 +77,17 @@ testData = T.pack "467..114..\n\
 \......755.\n\
 \...$.*....\n\
 \.664.598..\n"
+
+testData2 :: Text 
+testData2 = T.pack "12.......*..\n\
+\+.........34\n\
+\.......-12..\n\
+\..78........\n\
+\..*....60...\n\
+\78..........\n\
+\.......23...\n\
+\....90*12...\n\
+\............\n\
+\2.2......12.\n\
+\.*.........*\n\
+\1.1.......56\n"
